480d02f34ea9ab3d9fb4f390554e88e20b5161d1
Max Wofford <max@maxwofford.com>
2025-03-12 01:47:10 -0400

    Switch screenshot to using CDN
    Co-Authored-By: john cohn <johncohnvt@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # onboard-stats
 
 Simple program written by j cohn (with chatgpt) to plat onboard stats
+
+Want to run this locally? Check out `example.env` for what you need to spin up.
--- screenshot.js
+++ screenshot.js
@@ -2,7 +2,6 @@ import { IncomingWebhook } from "@slack/webhook";
 import puppeteer from "puppeteer";
 import path from "path";
 import { format } from "date-fns";
-import cron from "node-cron";
 
 const { SLACK_WEBHOOK_URL, DOMAIN } = process.env;
 const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
@@ -46,19 +45,34 @@ const takeScreenshot = async (url) => {
   return timestamp;
 };
 
+const uploadFileToCDN = async (file) => {
+  console.log("Uploading file to CDN", file)
+  const uploadText = await fetch('https://cdn.hackclub.com/api/v3/new', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer beans',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify([file])
+  }).then(res => res.text())
+  console.log(uploadText)
+  const uploadData = JSON.parse(uploadText)
+
+  return uploadData.files[0].deployedUrl
+}
+
 const sendHook = async () => {
   const timestamp = await takeScreenshot(DOMAIN + "/home");
+  const imageUrl = await uploadFileToCDN(`${DOMAIN}/graphs/graphs_${timestamp}.png`)
   await webhook.send({
     text: "Here's the daily OnBoard Stats :onboard:",
     attachments: [
       {
         title: `<${DOMAIN}|OnBoard Stats> sent at ${timestamp}`,
-        image_url: `${DOMAIN}/graphs/graphs_${timestamp}.png`,
+        image_url: imageUrl,
       },
     ],
   });
 };
-
-cron.schedule("0 6 * * *", sendHook);
 
 sendHook();
